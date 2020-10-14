fe19e003d6b06f64a75249dc7895f39bd5c01867
Jacob Harvey <66391376+jacoblharvey@users.noreply.github.com>
2020-10-13 20:06:20 -0500

    Update rangefinder example
    Introduces two servos that react to the distance threshold in addition to the onbaord LED. Flags a bug in distance calculation. General formatting like removing unecessary comments

--- apolloRobot/rangefinderEx/rangefinderEx.ino
+++ apolloRobot/rangefinderEx/rangefinderEx.ino
@@ -1,23 +1,30 @@
-/*
- * Created by ArduinoGetStarted.com
+/**
+ * Created by ArduinoGetStarted.com, modified by group Apollo
  *
  * This example code is in the public domain
  *
  * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-ultrasonic-sensor-led
  */
+#include <Servo.h>
 
-const int TRIG_PIN = 3;  // trig pin connects to pin 3 on
-const int ECHO_PIN = 4;  // echo pin connects to pin 4 on
+const int TRIG_PIN = 3;
+const int ECHO_PIN = 4;
 const int LED_PIN  = 13; // onboard LED
-const int DISTANCE_THRESHOLD = 20; // centimeters
+const int DISTANCE_THRESHOLD = 15; // centimeters
 
 float duration_us, distance_cm;
 
+Servo servo1, servo2;
+
 void setup() {
-  Serial.begin (9600);       // initialize serial port
-  pinMode(TRIG_PIN, OUTPUT); // set arduino pin to output mode
-  pinMode(ECHO_PIN, INPUT);  // set arduino pin to input mode
-  pinMode(LED_PIN, OUTPUT);  // set arduino pin to output mode
+  Serial.begin (9600);
+  
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  
+  servo1.attach(6);
+  servo2.attach(9);
 }
 
 void loop() {
@@ -26,19 +33,23 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
-digitalWrite(12, HIGH);
-  // measure duration of pulse from ECHO pin
-  duration_us = pulseIn(ECHO_PIN, HIGH);
-  // calculate the distance
-  distance_cm = 0.017 * duration_us;
+  duration_us = pulseIn(ECHO_PIN, HIGH); // measure duration of pulse from ECHO pin and compute distance
+  distance_cm = 0.017 * duration_us;     // FIXME: output occasionally jumps to 2000+ cm in serial monitor
+                                         //        something to watch for when programming the robot
+  
   if(distance_cm < DISTANCE_THRESHOLD) {
-    digitalWrite(LED_PIN, HIGH); // turn on LED
+    digitalWrite(LED_PIN, HIGH);
+    servo1.write(25);
+    servo2.write(100);
   } else {
-    digitalWrite(LED_PIN, LOW);  // turn off LED
+    digitalWrite(LED_PIN, LOW);
+    servo1.write(100);
+    servo2.write(25);
   }
+
   Serial.print("distance: ");
   Serial.print(distance_cm);
   Serial.println(" cm");
 
-  delay(500);
+  delay(250);
 }
