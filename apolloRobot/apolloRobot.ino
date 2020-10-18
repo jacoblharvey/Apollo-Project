873d9bf330cd751d3896067d56d47a1cd5932632
Jacob Harvey <66391376+jacoblharvey@users.noreply.github.com>
2020-10-18 16:04:57 -0500

    Release apolloRobot version 0.5.2
    Sets pins for servos and creates general variables for easier modification, flags new TODOs and FIXMEs

--- apolloRobot/apolloRobot.ino
+++ apolloRobot/apolloRobot.ino
@@ -1,7 +1,7 @@
 /**
  * Authors: Sharif Akil, Pamela Hernandez Villalba, Helena Holland, Brent Fairchild, Jacob Harvey
  * Date:    2020/10/18
- * Version: 0.5.1
+ * Version: 0.5.2
  * 
  * This program provides directions for our robot in order to complete the tasks
  * specified in https://github.com/jacoblharvey/Apollo-Project/blob/main/README.md
@@ -12,7 +12,14 @@
 // rangefinder
 const int TRIG_PIN = 3;
 const int ECHO_PIN = 4;
-const int LED_PIN  = 13; // onboard LED
+// wheels
+const int RIGHT_PIN = 8;
+const int LEFT_PIN = 6;
+// spikes
+const int FRONT_PIN = 1;
+const int REAR_PIN = 10;
+// cube drop
+const int DROP_PIN = 7;
 // TODO: verify accuracy of rangefinder before final release
 int distanceThreshold, inRange, phaseStep;
 float duration_us, distance_cm;
@@ -24,13 +31,12 @@ void setup() {
     // rangefinder pins
     pinMode(TRIG_PIN, OUTPUT);
     pinMode(ECHO_PIN, INPUT);
-    pinMode(LED_PIN, OUTPUT);
     // servo pins
-    leftWheel.attach(6); // TODO: specify pins depending on which work best for layout
-    rightWheel.attach(9);
-    frontSpike.attach(4);
-    rearSpike.attach(5);
-    cubeDrop.attach(7);
+    rightWheel.attach(RIGHT_PIN);
+    leftWheel.attach(LEFT_PIN);
+    frontSpike.attach(FRONT_PIN);
+    rearSpike.attach(REAR_PIN);
+    cubeDrop.attach(DROP_PIN);
 }
 
 int checkDistance(int phase) {
@@ -41,28 +47,30 @@ int checkDistance(int phase) {
     digitalWrite(TRIG_PIN, LOW);
 
     duration_us = pulseIn(ECHO_PIN, HIGH); // measure duration of pulse from ECHO pin and compute distance
-    distance_cm = 0.017 * duration_us;     // FIXME: output occasionally jumps to ~2000 cm in serial monitor
-    
+    distance_cm = 0.017 * duration_us;     // FIXME: serial monitor output fluctuates
+
     switch(phase) {
         case 1:
-            distanceThreshold = 30; // centimeters
-            if(distance_cm <= distanceThreshold) { // 1 for within threshold, 0 for out of threshold
-                Serial.println(distance_cm);
+            distanceThreshold = 20; // TODO: switch to microseconds to remove need for distance calculation
+            if(distance_cm >= distanceThreshold) { // 1 for within threshold, 0 for out of threshold
+                Serial.print(distance_cm);
+                Serial.println(" cm");
                 condition = 1;
             } else {
-                Serial.println(distance_cm);
+                Serial.print(distance_cm);
+                Serial.println(" cm");
                 condition = 0;
             }
             break;
-        case 2:
-            break;
         case 3:
-            distanceThreshold = 20; // centimeters
-            if(distance_cm >= distanceThreshold) { // 0 for within threshold, 1 for out of threshold
-                Serial.println(distance_cm);
+            distanceThreshold = 10; // centimeters
+            if(distance_cm <= distanceThreshold) { // 0 for within threshold, 1 for out of threshold
+                Serial.print(distance_cm);
+                Serial.println(" cm");
                 condition = 1;
             } else {
-                Serial.println(distance_cm);
+                Serial.print(distance_cm);
+                Serial.println(" cm");
                 condition = 0;
             }
             break;
@@ -83,7 +91,8 @@ void loop() {
      */
     inRange = checkDistance(1);
     phaseStep = 1;
-    rearSpike.write(25); // start phase 1 with spike down
+//rearSpike.write(25); TODO: lower spike before moving
+    // main movement loop for phase 1
     while(inRange == 1) {
         switch(phaseStep) {
             case 1:
@@ -101,6 +110,7 @@ void loop() {
                 break;
             case 3:
                 inRange = checkDistance(1);
+                Serial.print("Checking distance ... ");
                 Serial.println(inRange);
                 delay(250);
                 if(inRange != 0) {
@@ -112,7 +122,8 @@ void loop() {
                 break;
         }
     }
-    rearSpike.write(100);
+//rearSpike.write(100); // ver 0.5.1: once inRange = 0, program skips while loop and repeatedly sets servo to 25 then 100
+
     // TODO: implement phase 2
     /**
      * Phase 2: drop cube at drop off point
