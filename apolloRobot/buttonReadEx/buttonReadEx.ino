9556261eea7f5a579f3f06ca54817b5370c37ea0
sharifakil <72765840+sharifakil@users.noreply.github.com>
2020-10-24 20:16:35 -0500

    Release buttonRead

--- apolloRobot/buttonReadEx/buttonReadEx.ino
+++ apolloRobot/buttonReadEx/buttonReadEx.ino
@@ -1,9 +1,63 @@
+
+int stateC;
+int timerC;
+
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(6, OUTPUT); //WHITE
+  pinMode(7, OUTPUT); //RED
+  pinMode(8, INPUT_PULLUP); //BUTTON
+
+  stateC = 0;
+  timerC = 0;
+
+  Serial.println("Program Start");
+}
+
+void stateMachineC(){ //gets called 10 times per second
+
+  if(timerC > 0){
+    timerC = timerC - 1;
+  } else{
+    switch(stateC){
+      case 0:
+        digitalWrite(6, HIGH); // turn White LED on
+        if(digitalRead(8) == 0){
+        // button is pressed
+        digitalWrite(6, LOW); // turn LED off
+        stateC = 1;
+        timerC = 10;
+        }
+        break;
+      case 1:
+        digitalWrite(6, LOW); // turn LED off
+         if(digitalRead(8) == 0){
+        // button is pressed
+        digitalWrite(6, HIGH); // turn White LED on
+        stateC = 0;
+        timerC = 10;
+        }
+        break;
+
+  }
+  }
 
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void oncePer100mS(){ //gets called 10 times per second
 
+  stateMachineC();
 }
+void loop(){
+  int i;
+  delay(100); //this is the only thing that slows us down
+  oncePer100mS();
+
+
+ i=digitalRead(8);
+}
+
+
+
+ 
